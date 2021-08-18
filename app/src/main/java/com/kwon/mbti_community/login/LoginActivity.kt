02f971715b8731828575cdba544f60814acce8cd
KwonGeneral <thtm9020@gmail.com>
2021-08-18 14:29:29 +0900

    0818 update1

---
+++ app/src/main/java/com/kwon/mbti_community/login/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.kwon.mbti_community.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kwon.mbti_community.R
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        login_btn.setOnClickListener {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/kwon/mbti_community/signup/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.kwon.mbti_community.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kwon.mbti_community.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
